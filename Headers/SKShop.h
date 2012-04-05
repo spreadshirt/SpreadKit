ea4b45e40160821f1276074722c420fce6c61ad3
Sebastian Marr <marr@spreadshirt.net>
2012-04-05 13:38:17 +0000

    Created Shop Entity Type

---
+++ Headers/SKShop.h
@@ -0,0 +1,21 @@
+//
+//  SKShop.h
+//  SpreadKit
+//
+//  Created by Sebastian Marr on 05.04.12.
+//  Copyright (c) 2012 sprd.net AG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class SKUser;
+
+@interface SKShop : NSObject
+
+@property (strong) NSString * url;
+@property (strong) NSString * identifier;
+@property (strong) NSString * name;
+@property (strong) SKUser * user;
+@property (strong) NSSet * products;
+
+@end
---
+++ Implementation/SKShop.m
@@ -0,0 +1,20 @@
+//
+//  SKShop.m
+//  SpreadKit
+//
+//  Created by Sebastian Marr on 05.04.12.
+//  Copyright (c) 2012 sprd.net AG. All rights reserved.
+//
+
+#import "SKShop.h"
+#import "SKUser.h"
+
+@implementation SKShop
+
+@synthesize url;
+@synthesize identifier;
+@synthesize name;
+@synthesize user;
+@synthesize products;
+
+@end
