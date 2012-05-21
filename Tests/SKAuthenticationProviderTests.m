b94b5c5d72ae7f9e02475bd8f431cd197e2fcb8c
Sebastian Marr <marr@spreadshirt.net>
2012-05-21 12:34:59 +0000

    fix tests

---
+++ Tests/SKAuthenticationProviderTests.m
@@ -0,0 +1,18 @@
+//
+//  SKAuthenticationProviderTests.m
+//  SpreadKit
+//
+//  Created by Sebastian Marr on 10.05.12.
+//  Copyright (c) 2012 sprd.net AG. All rights reserved.
+//
+
+#import "SKAuthenticationProviderTests.h"
+
+@implementation SKAuthenticationProviderTests
+
+- (void)testAuthenticationHeader
+{
+    
+}
+
+@end
