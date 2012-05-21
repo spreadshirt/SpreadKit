c9e445a7bd5b8c8858a02dc3d0364574524cf03c
Sebastian Marr <marr@spreadshirt.net>
2012-05-21 12:35:00 +0000

    SprdAuth header generation

---
+++ Headers/SKAuthenticationProvider.h
@@ -0,0 +1,21 @@
+//
+//  SKAuthenticationProvider.h
+//  SpreadKit
+//
+//  Created by Sebastian Marr on 04.05.12.
+//  Copyright (c) 2012 sprd.net AG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKAuthenticationProvider : NSObject
+
+// calculates the SprdAuth Authorization Header, Session ID is optional
++ (NSString *)authorizationHeaderFromApiKey:(NSString *)theApiKey
+                                  andSecret:(NSString *)theSecret 
+                                     andURL:(NSString *)theURL 
+                                  andMethod:(NSString *)theMethod
+                               andTimeStamp:(NSString *)theTimestamp
+                               andSessionId:(NSString *)theSessionId;
+
+@end
--- Tests/SKAuthenticationProviderTests.m
+++ Tests/SKAuthenticationProviderTests.m
@@ -6,13 +6,36 @@
 //  Copyright (c) 2012 sprd.net AG. All rights reserved.
 //
 
-#import "SKAuthenticationProviderTests.h"
+#import <GHUnitIOS/GHUnit.h>
+#import "SKAuthenticationProvider.h"
+
+@interface SKAuthenticationProviderTests : GHTestCase
+
+@end
 
 @implementation SKAuthenticationProviderTests
 
-- (void)testAuthenticationHeader
+- (void)testAuthorizationHeader
 {
+    NSString *header = [SKAuthenticationProvider authorizationHeaderFromApiKey:@"123456789" 
+                                                                     andSecret:@"987654321" 
+                                                                        andURL:@"http://localhost:8080/api/v1/users/42/productPriceCalculator" 
+                                                                     andMethod:@"POST"
+                                                                  andTimeStamp:@"1240575575156" 
+                                                                  andSessionId:nil];
     
+    NSString *targetHeader = @"SprdAuth apiKey=\"123456789\", data=\"POST http://localhost:8080/api/v1/users/42/productPriceCalculator 1240575575156\", sig=\"70aab75c0b6217c2aff1f896bd4081fe30920911\"";
+    GHAssertEqualStrings(header, targetHeader, @"Authorization header without session id should be correct");
+    
+    header = [SKAuthenticationProvider authorizationHeaderFromApiKey:@"123456789" 
+                                                           andSecret:@"987654321" 
+                                                              andURL:@"http://localhost:8080/api/v1/users/42/productPriceCalculator" 
+                                                           andMethod:@"POST"
+                                                        andTimeStamp:@"1240575575156" 
+                                                        andSessionId:@"123"];
+    
+    targetHeader = @"SprdAuth apiKey=\"123456789\", data=\"POST http://localhost:8080/api/v1/users/42/productPriceCalculator 1240575575156\", sig=\"70aab75c0b6217c2aff1f896bd4081fe30920911\", sessionId=\"123\"";
+    GHAssertEqualStrings(header, targetHeader, @"Authorization header with session id should be correct");
 }
 
 @end
