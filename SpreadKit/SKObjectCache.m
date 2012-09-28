bb9abb44d356d9c8aa334d6734074af5095a1c37
Sebastian Marr <marr@spreadshirt.net>
2012-09-28 08:34:20 +0000

    object cache sets references to already cached objects

--- SpreadKit/SKObjectCache.m
+++ SpreadKit/SKObjectCache.m
@@ -34,18 +34,34 @@ NSString* const SKObjectCacheKeyPathKey = @"SKObjectCacheKeyPathKey";
 
 - (void)addObject:(id)object
 {
-    if ([object respondsToSelector:@selector(url)])
-    {
+    if (!([object respondsToSelector:@selector(url)] && [object url])) {
+        // not cachable, no unique identifier
+        return;
+    }
+    
+    id cached = [self.cache objectForKey:[object url].absoluteString];
+    if (cached && ![self should:cached beReplacedBy:object]) {
+        // cached object is better
+        return;
+    } else {
         [self.cache setObject:object forKey:[object url].absoluteString];
         [self pointReferencesToNewObject:object];
-        // scan for attached entities
-        for (RTProperty *property in [[object class] rt_properties]) {
+        for (RTProperty *property in [object class].rt_properties) {
             id propertyObject = [object valueForKey:[property name]];
-            if ([[propertyObject class] isSubclassOfClass:[SKEntity class]]) {
-                if ([propertyObject respondsToSelector:@selector(url)]) {
-                    [self addReferenceForURL:[propertyObject url] byObject:object atKeyPath:property.name];
+            
+            NSString *type = [[property.typeEncoding  stringByReplacingOccurrencesOfString:@"@" withString:@""] stringByReplacingOccurrencesOfString:@"\"" withString:@""];
+            if ([NSClassFromString(type) isSubclassOfClass:[SKEntity class]]) {
+                
+                [self addReferenceForURL:[propertyObject url] byObject:object atKeyPath:property.name];
+                
+                if (propertyObject)
+                {
+                    id cachedProperty = [self.cache objectForKey:[propertyObject url].absoluteString];
+                    if (!cachedProperty || [self should:propertyObject beReplacedBy:cachedProperty]) {
+                        [object setValue:cachedProperty forKey:property.name];
+                    }
+                    [self addObject:propertyObject];
                 }
-                [self addObject:propertyObject];
             }
         }
     }
@@ -53,12 +69,14 @@ NSString* const SKObjectCacheKeyPathKey = @"SKObjectCacheKeyPathKey";
 
 - (void)addReferenceForURL:(NSURL *)url byObject:(id)object atKeyPath:(NSString *)keyPath
 {
-    NSMutableArray * objectsAndKeyPaths = [self.entityReferences objectForKey:url.absoluteString];
-    if (!objectsAndKeyPaths) {
-        objectsAndKeyPaths = [NSMutableArray array];
-        self.entityReferences[url.absoluteString] = objectsAndKeyPaths;
+    if (url) {
+        NSMutableArray * objectsAndKeyPaths = [self.entityReferences objectForKey:url.absoluteString];
+        if (!objectsAndKeyPaths) {
+            objectsAndKeyPaths = [NSMutableArray array];
+            self.entityReferences[url.absoluteString] = objectsAndKeyPaths;
+        }
+        [objectsAndKeyPaths addObject:@{SKObjectCacheObjectKey : object, SKObjectCacheKeyPathKey : keyPath}];
     }
-    [objectsAndKeyPaths addObject:@{SKObjectCacheObjectKey : object, SKObjectCacheKeyPathKey : keyPath}];
 }
 
 - (void)pointReferencesToNewObject:(id)object
@@ -76,5 +94,31 @@ NSString* const SKObjectCacheKeyPathKey = @"SKObjectCacheKeyPathKey";
     return [self.cache objectForKey:url.absoluteString];
 }
 
+- (BOOL)should:(id)objectA beReplacedBy:(id)objectB
+{
+    if ([objectA class] != [objectB class]) {
+        [NSException raise:NSInternalInconsistencyException format:@"Trying to compare objects of different types"];
+    }
+    
+    __block int objectANotNilProperties = 0;
+    [[objectA class].rt_properties enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        RTProperty *property = (RTProperty *)obj;
+        if ([objectA valueForKey:property.name])
+        {
+            objectANotNilProperties++;
+        }
+    }];
+    
+    __block int objectBNotNilProperties = 0;
+    [[objectB class].rt_properties enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        RTProperty *property = (RTProperty *)obj;
+        if ([objectB valueForKey:property.name])
+        {
+            objectBNotNilProperties++;
+        }
+    }];
+    
+    return objectBNotNilProperties > objectANotNilProperties;
+}
 
 @end
--- Tests/SKObjectCacheTests.m
+++ Tests/SKObjectCacheTests.m
@@ -30,9 +30,10 @@
     self.cache = nil;
 }
 
-- (void)testObjectCache
+- (void)testObjectUpdatingReferences
 {
     SKShop *shop = [[SKShop alloc] init];
+    shop.url = [NSURL URLWithString:@"http://shop.url"];
     
     SKUser *user = [[SKUser alloc] init];
     user.url = [NSURL URLWithString:@"http://foo.user"];
@@ -47,6 +48,7 @@
     SKUser *updatedUser = [[SKUser alloc] init];
     updatedUser.url = [NSURL URLWithString:@"http://foo.user"];
     updatedUser.name = @"bar";
+    updatedUser.memberSince = [NSDate date];
     
     GHAssertEquals(shop.user, user, nil);
     
@@ -54,6 +56,46 @@
     
     GHAssertEquals(shop.user, updatedUser, @"reference should have been updated");
     GHAssertEquals(updatedUser, [self.cache objectForUrl:updatedUser.url], nil);
+}
+
+- (void)testObjectCacheSettingReferencesOnNewObjects
+{
+    SKShop *shop = [[SKShop alloc] init];
+    shop.url = [NSURL URLWithString:@"http://foo.shop"];
+    shop.identifier = @"foo";
+    shop.name = @"foo";
+    
+    [self.cache addObject:shop];
+    
+    SKShop *shopStub = [[SKShop alloc] init];
+    shopStub.url = [NSURL URLWithString:@"http://foo.shop"];
+    
+    
+    SKArticle *article = [[SKArticle alloc] init];
+    article.url = [NSURL URLWithString:@"http://article.url"];
+    article.shop = shopStub;
+    
+    [self.cache addObject:article];
+    
+    GHAssertEquals(article.shop, shop, nil);
+    
+//    SKUser *userStub = [[SKUser alloc] init];
+//    userStub.url = [NSURL URLWithString:@"http://foo.user"];
+//    userStub.identifier = @"foo";
+//    
+//    [self.cache addObject:userStub];
+//    
+//    SKUser *user = [[SKUser alloc] init];
+//    user.url = [NSURL URLWithString:@"http://foo.user"];
+//    user.identifier = @"foo";
+//    user.name = @"foo";
+//    
+//    SKDesign *design = [[SKDesign alloc] init];
+//    design.user = user;
+//    
+//    [self.cache addObject:design];
+//    
+//    GHAssertEquals(design.user, user, nil);
 }
 
 @end
